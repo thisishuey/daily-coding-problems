f87edb0e08cec8d9256dd166adfe418977aa716f
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-06-13 00:25:41 +0000

    2019-06-12 Problem

---
+++ 2019/06/12/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+console.log("2019-06-12 Dailing Coding Problem");
+
+let numbers = [];
+
+try {
+  numbers = JSON.parse(process.argv[2]);
+} catch (exception) {
+  console.error("Please provide an array of numbers.");
+  console.error(exception);
+}
+const k = process.argv[3];
+
+let solved = false;
+
+for (let number of numbers) {
+  solved = numbers.indexOf(k - number) >= 0;
+  if (solved) {
+    console.log(
+      `Match: ${number} + ${numbers[numbers.indexOf(k - number)]} = ${k}`
+    );
+    break;
+  }
+}
+
+console.log(solved);
+return solved;
